eacaf6ce36430c44aa556e8d40a432e8b2f2fb8c
Alexander Whitmore <ale15048@tjx.com>
2021-07-20 12:30:15 -0400

    Add comments

--- replace.ps1
+++ replace.ps1
@@ -1,1 +1,8 @@
+<#
+    1. Find markdown file
+    2. Replace version number
+    3. Set new version number 
+#> 
+
+# On script execution, the markdown tabe is updated and is displayed in ADO markdown widget
 (Get-Content ./dashboardTable.md).replace('0.0.0', '0.0.1') | Set-Content './dashboardTable.md'
